58277c5ba53302a0c3818b309120f7711e4b844f
prathamsaxena1 <pratham.s@turing.com>
2025-06-22 17:12:31 +0530

    interaction 2 Conversation 7_B

--- frontend/hotel-management-client/src/pages/Dashborad/EditHotel.jsx
+++ frontend/hotel-management-client/src/pages/Dashborad/EditHotel.jsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import './EditHotel.css'
 
 const EditHotel = () => {
   const { id } = useParams();
